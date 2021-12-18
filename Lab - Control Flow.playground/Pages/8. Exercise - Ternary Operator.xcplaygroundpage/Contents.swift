4596db4cf4a95a80cccc83b593b71b0fcec5c0bb
Arjun Prabhakar <arjun94022@gmail.com>
2021-12-17 17:34:03 -0800

    Delete Lab - Control Flow.playground directory

--- Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++
@@ -1,7 +0,0 @@
-/*:
- ## Exercise - Ternary Operator
- 
- Refactor the code below so that `largest` is declared and assigned to in one line using the ternary operator.
- */
-
-//: [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
