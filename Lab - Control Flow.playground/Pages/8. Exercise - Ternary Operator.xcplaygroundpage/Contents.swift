990a2b684539bbe2c9e80032afef63819356d2f9
Arjun Prabhakar <arjun94022@gmail.com>
2021-12-17 16:02:04 -0800

    Add files via upload

---
+++ Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+/*:
+ ## Exercise - Ternary Operator
+ 
+ Refactor the code below so that `largest` is declared and assigned to in one line using the ternary operator.
+ */
+
+//: [Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
